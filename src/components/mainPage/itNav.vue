96d491b529e1cb49c2e2dc3c7f317bc26543948b
酸奶羊驼 <2925497095@qq.com>
2022-08-03 20:02:27 +0800

    一键加群

--- src/components/mainPage/itNav.vue
+++ src/components/mainPage/itNav.vue
@@ -11,6 +11,17 @@
       />
     </div>
     <div class="content">
+      <span class="qqGroup" v-show="isPC">
+        <a
+          target="blank"
+          href="https://qm.qq.com/cgi-bin/qm/qr?k=QCHusRvSWhuXRVydCnLIBR4XjThzsYmI&jump_from=webapi"
+          ><img
+            border="0"
+            src="//pub.idqqimg.com/wpa/images/group.png"
+            alt="2022爱特工作室纳新群&lt;$"
+            title="2022爱特工作室纳新群&lt;$"
+        /></a>
+      </span>
       <li
         :class="{ choose: currentIdx == index }"
         id="li"
@@ -36,50 +47,64 @@ export default {
         { name: "历史团队" },
       ],
       currentIdx: -1, //当前位置的下标
+      isPC: false, //是否为电脑端
+      isAndroid: Boolean,
+      isIOS: Boolean,
+    };
+  },
+  created() {
+    let u = navigator.userAgent;
+    this.isAndroid = u.indexOf("Android") > -1 || u.indexOf("Adr") > -1; //判断是否是 android终端
+    this.isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //判断是否是 iOS终端
+    if (!this.isAndroid && !this.isIOS) {
+      this.isPC = true;
     }
+    console.log("是否是Android：", this.isAndroid); //true,false
+    console.log("是否是iOS：", this.isIOS);
+    console.log("是否是pc：", this.isPC);
   },
   methods: {
     returnTop() {
-      this.currentIdx = -1
+      this.currentIdx = -1;
       window.scrollTo({
         top: 0,
         behavior: "smooth",
-      })
+      });
     },
     scrollbarMove(index) {
-      var windowWidth = document.documentElement.clientWidth
-      this.currentIdx = index
+      var windowWidth = document.documentElement.clientWidth;
+      this.currentIdx = index;
       switch (index) {
         case 0:
           window.scrollTo({
             top: 0.53 * windowWidth,
             behavior: "smooth",
-          })
-          break
+          });
+          break;
         case 1:
           window.scrollTo({
             top: 0.871 * windowWidth,
             behavior: "smooth",
-          })
-          break
+          });
+          break;
         case 2:
           window.scrollTo({
             top: 1.435 * windowWidth,
             behavior: "smooth",
-          })
-          break
+          });
+          break;
         case 3:
           window.scrollTo({
             top: 1.896 * windowWidth,
             behavior: "smooth",
-          })
-          break
+          });
+          break;
         default:
-          break
+          break;
       }
     },
   },
-}
+};
 </script>
 
 <style scoped>
@@ -125,25 +150,30 @@ li:hover {
 .choose {
   color: #e19f73;
 }
-
+.qqGroup {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* transform: scale(1.2); */
+}
 /* 手机 */
-@media screen and (max-width: 768px){
+@media screen and (max-width: 768px) {
   .itNavBg {
-  height: 8vw;
-}
-.itNav {
-  width: 80vw;
-  height: 8vw;
-}
-.logo {
-  width: 20vw;
-  margin-top: 5px;
-}
-.content {
-  width: 50vw;
-}
-li {
-  font-size: 2.5vw;
-}
+    height: 8vw;
+  }
+  .itNav {
+    width: 80vw;
+    height: 8vw;
+  }
+  .logo {
+    width: 20vw;
+    margin-top: 5px;
+  }
+  .content {
+    width: 50vw;
+  }
+  li {
+    font-size: 2.5vw;
+  }
 }
 </style>
