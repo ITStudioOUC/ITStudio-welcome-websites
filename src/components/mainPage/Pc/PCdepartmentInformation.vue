d939408eaedf926706e58588d1b617a48b70eb66
白 <3401797899@qq.com>
2025-07-06 15:23:15 +0800

    部门介绍静态化

--- src/components/mainPage/Pc/PCdepartmentInformation.vue
+++ src/components/mainPage/Pc/PCdepartmentInformation.vue
@@ -69,17 +69,49 @@ export default {
   components: "",
   data() {
     return {
-      departmentInfo: [], //将获取完后的数组再存放
+      departmentInfo: [
+        {
+          department_cn: "程序部",
+          department_en: "PROGRAM",
+          content: "程序部门主要职能为基于Django,ASP.NET,PHP,JSP等技术,应用Python, C#，Java, c+＋等语言开发应用程序，完成与SQL Server，MySQL等数据库的交互，设计对外API接口等。",
+          introduction: "程序部，所谓之程序，乃是后端开发的意思，并非常规意义上常见的桌面应用程序，而是更加偏向计算机网络方向的学习核心。主要能够学到的内容有后端接口、数据库以及服务器的相关一系列操作，这也是每一个互联网应用中必要的部分，会比较有挑战性喔，请做好无限学习的准备！",
+          background: require("../../../assets/programBg.png"),
+        },
+        {
+          department_cn: "Web开发",
+          department_en: "WEBBASED",
+          content: "Web开发部门利用html,Css.JavaScript为主要语言来对网站表现层与结构层等前台部分做出完关构造，在实现设计师要带给用户上。的视觉冲击之外还能保证与后台交互实现网站对用户功能,从而让网站突破实用性和吸引力。",
+          introduction: "Web开发可以暂时理解成写网页，例如你现在浏览的网页，你所能看到的部分（网页上的文字图片、各种动画效果）都是我们的成员在Web开发部门学习后的作品。\r\n  写网页，我们这里需要用到三部分知识：Html、CSS、Javascript，大家大家可以看到没有涉及C语言、Python等，因此对零基础的同学较为友好。在进入社团前，我们期望你们预习基础的Html知识，掌握基本的标签。在进入社团后，我们每周都会组织授课，按照上述的顺序讲课，在课下我们会布置相关的练习，需要你在上课后，运用学习的知识完成练习。\r\n  因此，虽然Web开发对零基础的同学比较友好，但是大家也可以看到进来之后大家也需要不断的学习，这样才能向写一个网页这一目标不断前进嘛！",
+          background: require("../../../assets/webBg.png"),
+        },
+        {
+          department_cn: "UI设计",
+          department_en: "DESIGN",
+          content: "UI设计部门主要使用Photoshop, Illustrator等软件，对网页、APP等界面进行设计，并负责各类视频制作、剪辑、特效制作的工作。",
+          introduction: "UI设计是什么？\r\n这或许是每个萌新都好奇的问题。\r\n(不会就百——)\r\n通俗来讲，\r\nUI就是决定APP界面、网页等长啥样的部门，\r\n重在【设计】\r\n(包含人机交互、操作逻辑、界面美观的整体设计。)\r\n那就不得不说设计师们与Adobe全家桶等不得不说的故事了。\r\n(想听吗？想听就来！报！名！)\r\n\r\n来UI部门，\r\n你可以畅游在Adobe的海洋，\r\n你可以体会格式塔心理的美学，\r\n你可以感受设计之道……\r\n(反正来就vans 加入我们吧！)",
+          background: require("../../../assets/uiBg.png"),
+        },
+        {
+          department_cn: "游戏开发",
+          department_en: "GAME DEVELOP",
+          content: "爱特游戏部门是2018年新成立的部门。专注于2D游戏的开发。游戏以游戏性为本,以社交性为辅，以商业性为底。他们用心为自己的理想开辟道路。",
+          introduction: "大一秋季学期期间会教给新生Unity的一些简单的使用，包括场景实例搭建和脚本代码编写，让新生具备自主开发简单的小游戏的能力。期间会有三次项目作业，难度逐步提升。\r\n大一春季学期期间则分组安排进行团队项目开发，每组成员在一学期内多人合作完成一个大型游戏项目的开发。",
+          background: require("../../../assets/uiBg.png"),
+        },
+        {
+          department_cn: "APP开发",
+          department_en: "APP DEVELOP",
+          content: "爱特工作室APP应用部门是在移动端高速发展下，于2017年成立的部门。Android应用开发基于Java等语言，负责编写应用客户端界面，完成客户端与服务端的信息应用开发部门扩展了社团的变量，使工作室的技术力量多样化。",
+          introduction: "APP部主要负责移动APP和微信小程序的设计与制作，在一年的学习你将动手实现5个不同类型的APP，拥有自主设计开发APP的能力，加入我们，让我们一起设计一款属于你自己的APP吧。",
+          background: require("../../../assets/appBg.png"),
+        },
+      ], //将获取完后的数组再存放
       pages: [], //存放盒子的数组
       id: "",
       departmentInformation: [],
       DEL_departmentInfo: [
         {
-          department_cn: "程序部",
-           department_en: "hhhh",
-          content: "do you like what you see",
-          introduction: "红红火火恍恍惚惚或或或红红火火恍恍惚惚或或或或红红火火恍恍惚惚或或或或红红火火恍恍惚惚或或或或红红恍恍惚惚或或或或红红恍恍惚惚或或或或红红恍恍惚惚或或或或红红恍恍惚惚或或或或红红恍恍惚惚或或或或红红恍恍惚惚或或或或红红恍恍惚惚或或或或红红恍恍惚惚或或恍恍惚惚或或或或红红恍恍惚惚或或或或红红恍恍惚惚或或或或红红恍恍惚惚或或或或红红或或红红火火恍恍惚惚或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或红红火火恍恍惚惚或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或",
-          background: require("../../../assets/programBg.png"),
+
         },
         // {
         //   name1: "游戏部",
@@ -109,25 +141,25 @@ export default {
     };
   },
   created() {
-    for (var i = 1; i <= 5; i++) {
-      this.$http
-        .get("/department_info/", {
-          params: {
-            id: i,
-          },
-        })
-        //回调函数
-        .then((res) => {
-          this.$data.departmentInformation.push(res.data.data);
-          // console.log(this.$data.departmentInformation);  关键试错点
-          if (this.$data.departmentInformation.length == 5) {
-            this.$data.departmentInfo = this.$data.departmentInformation;
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
+    // for (var i = 1; i <= 5; i++) {
+    //   this.$http
+    //     .get("/department_info/", {
+    //       params: {
+    //         id: i,
+    //       },
+    //     })
+    //     //回调函数
+    //     .then((res) => {
+    //       this.$data.departmentInformation.push(res.data.data);
+    //       // console.log(this.$data.departmentInformation);  关键试错点
+    //       if (this.$data.departmentInformation.length == 5) {
+    //         this.$data.departmentInfo = this.$data.departmentInformation;
+    //       }
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //     });
+    // }
   },
   // 监听数组变化，路由传参后执行预操作
   watch: {
@@ -198,7 +230,7 @@ export default {
   display: flex;
   align-items: center;
   background-attachment: fixed;
-  background-image: url(https://cdn.lmark.cc/it/static/background2.png);
+  background-image: url("../../../assets/background2.png");
   background-size: 100vw 100vh;
 }
 .bgInfo {
